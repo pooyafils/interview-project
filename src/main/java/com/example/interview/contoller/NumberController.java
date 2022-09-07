bc6e15c353ff24216a46486432f1a4a6ba08f216
pooyafils <pooyafils@gmail.com>
2022-09-07 06:57:31 -0700

    refactor service class

--- src/main/java/com/example/interview/contoller/NumberController.java
+++ src/main/java/com/example/interview/contoller/NumberController.java
@@ -2,10 +2,10 @@ package com.example.interview.contoller;
 
 import com.example.interview.exception.CustomExceptions;
 import com.example.interview.service.NumberService;
+import com.example.interview.service.NumberServiceImp;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name ="number controller")
 public class NumberController {
 @Autowired
-    NumberService numberService;
+NumberService numberService;
     @Operation(summary = "calculate matching numbers from array of numbers")
     @GetMapping("/checknumber")
     public ResponseEntity receiveNumbers(@RequestParam Integer sum,@RequestParam int[] numbers){
--- src/main/java/com/example/interview/service/NumberService.java
+++ src/main/java/com/example/interview/service/NumberService.java
@@ -1,34 +1,7 @@
 package com.example.interview.service;
 
-import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
-public class NumberService {
-    public ResponseEntity receiveNumbers( Integer sum,  int[] numbers){
-        ArrayList<int[]> arrayList=new ArrayList<>();
-        for(int i=0;i<numbers.length;i++){
-
-            for(int j=i+1;j<numbers.length;j++){
-                if(numbers[i]+numbers[j]==sum){
-                    int [] numbersEqualToSum={numbers[j],numbers[i]};
-                    arrayList.add(numbersEqualToSum);
-                }
-            }
-        }
-        if(arrayList.size()==0){
-            JSONObject zeroMatch = new JSONObject();
-            zeroMatch.put("message", "there is no numbers that can match the sum number");
-            zeroMatch.put("code",404);
-            return new ResponseEntity(zeroMatch.toMap(), HttpStatus.valueOf(404));
-        }
-        return new ResponseEntity(arrayList, HttpStatus.valueOf(200));
-    }
+public interface NumberService {
+    public ResponseEntity receiveNumbers(Integer sum, int[] numbers);
 }
